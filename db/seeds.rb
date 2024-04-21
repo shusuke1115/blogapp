4e5525c00c2f51b9b8e67841c2236de5e3c90b3a
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-04-21 14:08:44 +0900

    fakerによるダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Article.create({title: '新しい記事だよ',content: '素晴らしい記事です。' })
-
-Article.create({title: '素晴らしい記事だよ', content: 'あああああああ'})
+10.times do
+Article.create(
+  title:Faker::Lorem.sentence(word_count: 5),
+  content:Faker::Lorem.sentence(word_count: 100)
+)
+end
