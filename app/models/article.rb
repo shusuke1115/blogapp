e5153f00acec51db65b56557576ecab1b8f16b2e
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-05-03 10:50:00 +0900

    annotate rubocop bettererrorsの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
-  
+
   validates :title, presence: true
   validates :content, presence: true
 
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
