4fb756ac53ff69ce450136791e25499ac5b54d22
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-05-09 18:10:53 +0900

    いいねできるように いいねのsave

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
   resources :articles do
+#     resourcesとresourceの相違点について
+# 大きな違いは2つあり、それは
+# ①indexアクションがあるかないか
+# ②urlに:idを含むかどうか
+# です。
+# コメントにデストロイ機能をつけようとするとコメントにidがつく
+    # コメントは特定の記事に対して何個も追加できる
     resources :comments, only: [:new, :create]
+    # 自分にとって特定の記事にたいして一個しか言い値がないからresource
+    resource :like, only: [:create]
   end
   resource :profile, only: [:show, :edit, :update]
 end
